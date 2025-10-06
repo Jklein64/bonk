2e3fcb4faf2933921afbfecb14ca87673d221d3b
Jason Klein <jklein.64@verizon.net>
2025-10-06 03:24:46 -0400

    feat: send sim results to viz with SSE

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,115 @@
 #include <cstring>
-#include <vector>
-
-#include "npy/npy.h"
-#include "npy/tensor.h"
-#include "sim.h"
-
 #include <fmt/core.h>
 #include <httplib.h>
+#include <nlohmann/json.hpp>
+// #include <npy/npy.h>
+// #include <npy/tensor.h>
+
+#include "sim.h"
+
+class EventDispatcher {
+  public:
+    EventDispatcher() {}
+
+    void wait_event(httplib::DataSink& sink) {
+        std::unique_lock<std::mutex> lk(m_);
+        int id = id_;
+        cv_.wait(lk, [&] {
+            return cid_ == id;
+        });
+        sink.write(message_.data(), message_.size());
+    }
+
+    void send_event(const std::string& message) {
+        std::lock_guard<std::mutex> lk(m_);
+        cid_ = id_++;
+        message_ = message;
+        cv_.notify_all();
+    }
+
+  private:
+    std::mutex m_;
+    std::condition_variable cv_;
+    std::atomic_int id_{0};
+    std::atomic_int cid_{-1};
+    std::string message_;
+};
 
 int main() {
+    EventDispatcher ed;
     httplib::Server server;
     server.Get("/api/hi", [](const httplib::Request&, httplib::Response& res) {
         res.set_content("Hello World!", "text/plain");
     });
 
+    server.Get("/api/stream/audio", [&](const httplib::Request&, httplib::Response& res) {
+        res.set_header("Cache-Control", "no-cache");
+        res.set_header("Connection", "keep-alive");
+        res.set_chunked_content_provider("text/event-stream", [&](size_t, httplib::DataSink& sink) {
+            ed.wait_event(sink);
+            return true;
+        });
+    });
+
+    server.Post("/api/configure", [&](const httplib::Request& req, httplib::Response& res) {
+        SimParams params;
+        SimState initial_state;
+
+        try {
+            auto json_body = nlohmann::json::parse(req.body);
+            params = {
+                .physics_sample_rate = json_body["params"]["physicsSampleRate"],
+                .physics_block_size = json_body["params"]["physicsBlockSize"],
+                .audio_sample_rate = json_body["params"]["audioSampleRate"],
+                .audio_block_size = json_body["params"]["audioBlockSize"],
+                .viz_sample_rate = json_body["params"]["vizSampleRate"],
+                .viz_block_size = json_body["params"]["vizBlockSize"],
+                .mass = json_body["params"]["mass"],
+                .stiffness = json_body["params"]["stiffness"],
+                .damping = json_body["params"]["damping"],
+                .area = json_body["params"]["area"],
+            };
+            initial_state = {
+                .x = json_body["initialState"]["x"],
+                .v = json_body["initialState"]["v"],
+            };
+        } catch (std::exception e) {
+            res.status = 400;
+            res.body = e.what();
+            return;
+        }
+
+        std::thread([&ed, params, initial_state]() {
+            Sim sim;
+            bool should_step = true;
+            sim.configure(params, initial_state);
+            sim.set_audio_callback([&](auto& audio_block) {
+                // TODO json is lossy when storing floats as strings!
+                nlohmann::json json_block(audio_block);
+                ed.send_event(fmt::format("data: {}\n\n", json_block.dump()));
+
+                double avg_power = 0;
+                for (auto& sample : audio_block) {
+                    avg_power += sample * sample;
+                }
+
+                // Stop when a block is silent (< -60 dB power)
+                if (avg_power < 1e-6) {
+                    should_step = false;
+                }
+            });
+
+            double dt = 1. / params.physics_sample_rate;
+            while (should_step) {
+                sim.step(dt);
+            }
+        }).detach();
+
+        res.set_content("Nice!", "text/plain");
+    });
+
     server.listen("0.0.0.0", 3001);
 }
-// int main() {
-//     Sim sim;
-
-//     SimParams params = {
-//         .physics_sample_rate = 1000000,
-//         .physics_block_size = 512,
-//         .audio_sample_rate = 48000,
-//         .audio_block_size = 128,
-//         .viz_sample_rate = 30,
-//         .viz_block_size = 5,
-//         // These sound okay...
-//         .mass = 0.005,
-//         .stiffness = 3000,
-//         .damping = 0.12,
-//         .area = 1,
-//     };
-//     SimState initial_state = {
-//         .x = 2,
-//         .v = 0,
-//     };
-//     std::vector<double> all_blocks;
-//     sim.configure(params, initial_state);
-//     bool should_step = true;
-//     sim.set_audio_callback([&](auto& audio_block) {
-//         double avg_power = 0;
-//         for (auto& sample : audio_block) {
-//             all_blocks.push_back(sample);
-//             avg_power += sample * sample;
-//         }
-
-//         // Stop when a block is silent (< -60 dB power)
-//         if (avg_power < 1e-6) {
-//             should_step = false;
-//         }
-//     });
-
-//     double dt = 1. / params.physics_sample_rate;
-//     while (should_step) {
-//         sim.step(dt);
-//     }
 
 //     // Save to numpy file
 //     std::vector<size_t> shape({all_blocks.size()});
--- viz/src/main.tsx
+++ viz/src/main.tsx
@@ -1,33 +1,13 @@
 import * as THREE from "three";
 import { useRef, useState, StrictMode, useMemo } from "react";
 import { createRoot } from "react-dom/client";
-import { Canvas, useFrame, type ThreeElements } from "@react-three/fiber";
+import { Canvas, type ThreeElements } from "@react-three/fiber";
 import "./index.css";
-
-function Box(props: ThreeElements["mesh"]) {
-  const ref = useRef<THREE.Mesh>(null!);
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  useFrame((state, delta) => (ref.current.rotation.x += delta));
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? getColor("sky-magenta") : getColor("tiffany-blue")} />
-    </mesh>
-  );
-}
 
 function Wall(props: ThreeElements["mesh"]) {
   return (
     <mesh {...props}>
-      <boxGeometry args={[0.5, 3, 3]} />
+      <boxGeometry args={[0.5, 3, 3]} computeBoundingBox={true} />
       <meshStandardMaterial color={getColor("english-violet")} />
     </mesh>
   );
@@ -37,8 +17,6 @@ function TrianglePrism(props: ThreeElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!);
   const [hover, setHover] = useState(false);
   const [clicking, setClicking] = useState(false);
-  const [preDragPoint, setPreDragPoint] = useState<THREE.Vector3 | undefined>();
-  const [displacement, setDisplacement] = useState(0);
 
   const geometry = useMemo(() => {
     let geometry = new THREE.BufferGeometry();
@@ -50,16 +28,9 @@ function TrianglePrism(props: ThreeElements["mesh"]) {
       new THREE.Vector3(0.5, 0, 0),
       new THREE.Vector3(0.5, 0, 1),
       new THREE.Vector3(0.5, Math.sqrt(3) / 2, 0.5),
+      // Move so that the object's center is at the local origin
     ].map((v) => v.add(new THREE.Vector3(-0.25, -Math.sqrt(3) / 6, -0.5)));
-
-    const vertices = new Float32Array([
-      ...vectors[0].toArray(), // v0
-      ...vectors[1].toArray(), // v1
-      ...vectors[2].toArray(), // v2
-      ...vectors[3].toArray(), // v3
-      ...vectors[4].toArray(), // v4
-      ...vectors[5].toArray(), // v5
-    ]);
+    const vertices = new Float32Array(vectors.flatMap((v) => v.toArray()));
 
     const indices = [
       ...[0, 1, 2],
@@ -84,6 +55,15 @@ function TrianglePrism(props: ThreeElements["mesh"]) {
   if (hover) color = getColor("lavender-pink");
   if (clicking) color = getColor("sky-magenta");
 
+  function onClickDone() {
+    setClicking(false);
+    // Hard-coded rest position of 1
+    const displacement = ref.current.position.x - 1;
+    if (Math.abs(displacement) > 1e-8) {
+      configureSim(displacement);
+    }
+  }
+
   return (
     <mesh
       ref={ref}
@@ -91,23 +71,20 @@ function TrianglePrism(props: ThreeElements["mesh"]) {
       onPointerOver={() => setHover(true)}
       onPointerLeave={() => {
         setHover(false);
-        // setClicking(false);
-        setPreDragPoint(undefined);
+        if (clicking) {
+          onClickDone();
+        }
       }}
-      onPointerDown={(e) => {
+      onPointerDown={() => {
         setClicking(true);
-        setPreDragPoint(e.point);
       }}
-      onPointerUp={() => {
-        setClicking(false);
-        setPreDragPoint(undefined);
-      }}
+      onPointerUp={onClickDone}
       onPointerMove={(e) => {
-        if (clicking && preDragPoint) {
-          const d = e.point.sub(preDragPoint).x;
-          setDisplacement(d);
-          console.log(displacement);
-          ref.current.position.setX(preDragPoint.x + d);
+        if (clicking) {
+          // Hard-coded for now
+          if (e.point.x >= -0.375) {
+            ref.current.position.setX(e.point.x);
+          }
         }
       }}
       scale={clicking ? 1.1 : 1.0}
@@ -124,9 +101,6 @@ createRoot(document.getElementById("root")!).render(
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 20, 20]} angle={0.15} penumbra={1} decay={0.125} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0.25} intensity={Math.PI} />
-      {/* <directionalLight position={[1, 1, 1]} intensity={Math.PI} /> */}
-      {/* <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} /> */}
       <Wall position={[-1, 0, 0]} />
       <TrianglePrism position={[1, 0, 0]} />
     </Canvas>
@@ -137,4 +111,31 @@ function getColor(name: string) {
   const value = getComputedStyle(document.documentElement).getPropertyValue(`--color-${name}`);
   if (value === "") console.warn(`No color found with name ${name}.`);
   return value;
+}
+
+function configureSim(displacement: number) {
+  const params = {
+    physicsSampleRate: 1000000,
+    physicsBlockSize: 512,
+    audioSampleRate: 48000,
+    audioBlockSize: 128,
+    vizSampleRate: 30,
+    vizBlockSize: 5,
+    // These sound okay...
+    mass: 0.005,
+    stiffness: 3000,
+    damping: 0.12,
+    area: 1,
+  };
+  const initialState = {
+    x: displacement,
+    v: 0,
+  };
+
+  console.log({ params, initialState });
+  fetch("/api/configure", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ params, initialState }),
+  }).catch((err) => console.error(err));
 }
